83bf1c44a72d5a12cf8134f6fe97c0b198dab208
Zack Guard <zacharyguard@gmail.com>
2020-11-17 17:53:20 +0800

    Add some options

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const { WakaTimeClient, RANGE } = require("wakatime-client");
 const Octokit = require("@octokit/rest");
+const { program } = require("commander");
 
 const {
   GIST_ID: gistId,
@@ -11,6 +12,17 @@ const {
 const wakatime = new WakaTimeClient(wakatimeApiKey);
 
 const octokit = new Octokit({ auth: `token ${githubToken}` });
+
+program
+  .version(require("./package.json").version)
+  .option(
+    "--format [FORMAT]",
+    "Control duration format. FORMAT can be 'short' or 'long'.",
+    "long"
+  )
+  .option("--include-percent", "Include a percentage after each bar.", false)
+  .option("--dry", "Print to stdout instead of uploading a gist.", false)
+  .parse(process.argv);
 
 async function main() {
   const stats = await wakatime.getMyStats({ range: RANGE.LAST_7_DAYS });
@@ -28,34 +40,46 @@ async function updateGist(stats) {
   const lines = [];
   for (let i = 0; i < Math.min(stats.data.languages.length, 5); i++) {
     const data = stats.data.languages[i];
-    const { name, percent, text: time } = data;
+    const { name, percent } = data;
 
-    const line = [
-      name.padEnd(11),
-      time.padEnd(14),
-      generateBarChart(percent, 21),
-      String(percent.toFixed(1)).padStart(5) + "%"
-    ];
+    let timeText;
+    if (program.format === "short") {
+      timeText = formatDuration(data.hours, data.minutes).padStart(8);
+    } else {
+      timeText = data.text.padEnd(14);
+    }
+
+    const line = [name.padEnd(11), timeText, generateBarChart(percent, 21)];
+
+    if (program.includePercent) {
+      line.push(String(percent.toFixed(1)).padStart(5) + "%");
+    }
 
     lines.push(line.join(" "));
   }
 
   if (lines.length == 0) return;
+  const content = lines.join("\n");
 
-  try {
-    // Get original filename to update that same file
-    const filename = Object.keys(gist.data.files)[0];
-    await octokit.gists.update({
-      gist_id: gistId,
-      files: {
-        [filename]: {
-          filename: `📊 Weekly development breakdown`,
-          content: lines.join("\n")
+  if (program.dry) {
+    process.stdout.write(content);
+    process.stdout.write("\n");
+  } else {
+    try {
+      // Get original filename to update that same file
+      const filename = Object.keys(gist.data.files)[0];
+      await octokit.gists.update({
+        gist_id: gistId,
+        files: {
+          [filename]: {
+            filename: `📊 Weekly development breakdown`,
+            content
+          }
         }
-      }
-    });
-  } catch (error) {
-    console.error(`Unable to update gist\n${error}`);
+      });
+    } catch (error) {
+      console.error(`Unable to update gist\n${error}`);
+    }
   }
 }
 
@@ -72,6 +96,10 @@ function generateBarChart(percent, size) {
   return [syms.substring(8, 9).repeat(barsFull), syms.substring(semi, semi + 1)]
     .join("")
     .padEnd(size, syms.substring(0, 1));
+}
+
+function formatDuration(h, m) {
+  return `${String(h).padStart(2)}h ${String(m).padStart(2)}m`;
 }
 
 (async () => {
